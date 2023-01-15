a4013f9eb3912e297298fe57946a5a6aee51bc02
joelvitorniino <joelprofessor13@gmail.com>
2023-01-15 14:46:13 -0300

    routes new

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,11 +1,16 @@
 import { Request, Response, Router } from "express";
 import { RegisterController } from "../controllers/RegisterController";
+import { LoginController } from "../controllers/LoginController";
 
 const registerController = new RegisterController();
+const loginController = new LoginController();
 
 const router = Router();
 
 router.get('/api/v1/register', registerController.index);
 router.post('/api/v1/register', registerController.store);
+
+router.get('/api/v1/getUser', loginController.getUser);
+router.post('/api/v1/login', loginController.index);
 
 export { router };
--- src/app/util/hashPassword.ts
+++
@@ -1,5 +0,0 @@
-import { hashSync } from "bcryptjs";
-
-export const hashPassword = (password: string) => {
-    return hashSync(password, 8);
-};
