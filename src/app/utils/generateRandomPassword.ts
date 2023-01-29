d9a7529ba433f91f177e1227246cbef59cc872c5
joelvitorniino <joelprofessor13@gmail.com>
2023-01-29 16:33:11 -0300

    create generateRandomPassword

---
+++ src/app/utils/generateRandomPassword.ts
@@ -0,0 +1,12 @@
+export const generateRandomPassword = ({ passwordLength = 12 }: { passwordLength?: number } = {}) => {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let password = "";
+
+    for (let i = 0; i <= passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    };
+
+    return password;
+};
