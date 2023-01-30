cabce8e9ac32d3af9f9953de8bf684fd4b2f0582
joelvitorniino <joelprofessor13@gmail.com>
2023-01-30 14:07:24 -0300

    create route to get accessToken in account google

--- src/app/modules/google/google.route.ts
+++ src/app/modules/google/google.route.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import fastifyPassport from '@fastify/passport';
-import { googleAuthenticateHandler } from './google.controller';
+import { googleAccessTokenHandler, googleAuthenticateHandler } from './google.controller';
 
 export const googleRoute = async (app: FastifyInstance) => {
     app.get('/auth/google/callback', {
         preValidation: fastifyPassport.authenticate('google', { scope: ['email', 'profile'] })
     }, googleAuthenticateHandler);
+
+    app.get('/auth/google/accessToken', googleAccessTokenHandler);
 };
