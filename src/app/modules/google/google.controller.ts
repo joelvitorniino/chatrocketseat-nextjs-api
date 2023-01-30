06d105e5f367ade896d449a53a96ff75f4bdca5e
joelvitorniino <joelprofessor13@gmail.com>
2023-01-30 14:07:01 -0300

    create googleAccessTokenHandler

--- src/app/modules/google/google.controller.ts
+++ src/app/modules/google/google.controller.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'; 
 import { prisma } from '../../utils/prisma';
+import { findUserByEmail } from '../register/register.service';
 import { app } from '../../server';
 
 interface User {
@@ -22,9 +23,22 @@ export const googleAuthenticateHandler = async (request: FastifyRequest, reply: 
         return reply.status(403);
     };
 
-    const { password, ...rest } = userGoogle
+    return reply.redirect('http://localhost:3000/chat_google')
+};
+
+export const googleAccessTokenHandler = async (request: FastifyRequest, reply: FastifyReply) => {
+    const user = request.user as User;
+
+    const userData = await findUserByEmail(user._json.email);
+    
+    if(!userData) {
+        return reply.status(403);
+    };
+
+    const { password, ...rest } = userData;
 
     return {
+        email: user._json.email,
         accessToken: app.jwt.sign(rest)
-    } && reply.redirect('http://localhost:3000/chat')
+    }
 };
